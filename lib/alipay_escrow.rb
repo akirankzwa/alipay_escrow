e0e6859122b5a6d9ebf3f333ab556313ab216575
Akira Nakazawa <nakazawa.akira@gmail.com>
2016-01-17 03:14:20 +0800

    Add base class

--- lib/alipay_escrow.rb
+++ lib/alipay_escrow.rb
@@ -1,5 +1,49 @@
-require "alipay_escrow/version"
+require 'alipay_escrow/version'
+require 'openssl'
+require 'base64'
+require 'active_support'
+require 'active_support/core_ext'
 
 module AlipayEscrow
-  # Your code goes here...
+  class Base
+    attr_reader :params, :key, :partner_id
+    attr_accessor :options
+
+    GATEWAY = 'https://mapi.alipay.com/gateway.do?'
+
+    def initialize(params, key, partner_id)
+      @params = params
+      @key = key
+      @partner_id = partner_id
+      @options = {}
+    end
+
+    def payment_url
+      options = Hash[payment_params.map { |k, v| [k.to_s, v] }]
+      str = options.sort.map { |item| item.join('=') }.join('&')
+      options.merge!(sign_type: 'RSA', sign: encrypt(str))
+      "#{GATEWAY}#{options.to_query}"
+    end
+
+    private
+
+    def payment_params
+      {
+        out_trade_no: params[:trade_no],
+        subject: params[:subject],
+        total_fee: params[:amount].to_s,
+        return_url: params[:return_url],
+        notify_url: params[:notify_url],
+        service: 'create_direct_pay_by_user',
+        partner: partner_id,
+        seller_id: partner_id,
+        payment_type: '1',
+        _input_charset: 'utf-8'
+      }
+    end
+
+    def encrypt(str)
+      Base64.strict_encode64(OpenSSL::PKey::RSA.new(key).sign('sha1', str))
+    end
+  end
 end
