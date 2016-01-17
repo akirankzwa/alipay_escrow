db402783d1153b96900cf07cdd715c20f7e5e018
Akira Nakazawa <nakazawa.akira@gmail.com>
2016-01-17 20:08:02 +0800

    Remove redundant require

--- lib/alipay_escrow/base.rb
+++ lib/alipay_escrow/base.rb
@@ -1,10 +1,3 @@
-require 'alipay_escrow/version'
-require 'openssl'
-require 'base64'
-require 'active_support'
-require 'active_support/core_ext'
-require 'httparty'
-
 module AlipayEscrow
   class Base
     attr_reader :params, :key, :partner_id
