631ba32a2dee8751ac5c0df5110217405799e123
Akira Nakazawa <nakazawa.akira@gmail.com>
2016-01-17 19:23:49 +0800

    Recreate Base class

--- lib/refund.rb
+++ lib/refund.rb
@@ -1,24 +1,5 @@
-require 'alipay_escrow/version'
-require 'openssl'
-require 'base64'
-require 'active_support'
-require 'active_support/core_ext'
-require 'httparty'
-
 module AlipayEscrow
-  class Refund
-    attr_reader :params, :key, :partner_id
-    attr_accessor :options
-
-    GATEWAY = 'https://mapi.alipay.com/gateway.do?'
-
-    def initialize(params, key, partner_id)
-      @params = params
-      @key = key
-      @partner_id = partner_id
-      @options = {}
-    end
-
+  class Refund < Base
     def refund_url
       options = Hash[refund_params.map { |k, v| [k.to_s, v] }]
       str = options.sort.map { |item| item.join('=') }.join('&')
@@ -39,10 +20,6 @@ module AlipayEscrow
         batch_num: 1,
         detail_data: "#{params[:trade_no]}^#{params[:amount]}^#{params[:reason]}"
       }
-    end
-
-    def encrypt(str)
-      Base64.strict_encode64(OpenSSL::PKey::RSA.new(key).sign('sha1', str))
     end
   end
 end
