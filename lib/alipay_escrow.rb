00514e0c0fd3541c30658e1caadfe405b4a764da
Akira Nakazawa <nakazawa.akira@gmail.com>
2016-01-20 21:52:26 +0800

    Use ActiveSupport::Autoload

--- lib/alipay_escrow.rb
+++ lib/alipay_escrow.rb
@@ -1,12 +1,12 @@
-require 'alipay_escrow/version'
-require 'alipay_escrow/base'
-require 'alipay_escrow/payment'
-require 'alipay_escrow/refund'
 require 'openssl'
 require 'base64'
 require 'active_support'
-require 'active_support/core_ext'
 require 'httparty'
 
 module AlipayEscrow
+  extend ActiveSupport::Autoload
+
+  autoload :Base
+  autoload :Payment
+  autoload :Refund
 end
